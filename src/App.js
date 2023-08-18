1f16640c627a8756eacdc0ce514b05909f118cd5
pranay2508 <bhatnagarpranay8@gmail.com>
2023-08-18 23:29:41 +0530

    router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import {createBrowserRouter , RouterProvider} from 'react-router-dom';
+import HomePage from './pages/Home';
+
+
+// http://example.com/products => example.com is the domain and /products is the path 
+const router1 = createBrowserRouter([
+  {path:'/pranay' ,element: <HomePage/>},
+  
+]);
+
 function App() {
-  return <div></div>;
+
+  return <RouterProvider router={router1}/>;
 }
 
 export default App;
+
+//you can control the path what you want to set and it will work accordingly 
+//so the router 1 is used as prop in router={router1} this is how it works
+//this is how we activate route
---
+++ src/pages/Home.js
@@ -0,0 +1,5 @@
+function HomePage() {
+  return <h1>my homepage</h1>;
+}
+
+export default HomePage;
